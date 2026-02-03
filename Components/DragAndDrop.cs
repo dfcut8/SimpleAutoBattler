7af4f31ac338bf283d2aceaec246d4cee7345177
dfcut8 <dfcut8@proton.me>
2026-02-03 10:38:34 -0500

    add inputs and drag implementation

--- Components/DragAndDrop.cs
+++ Components/DragAndDrop.cs
@@ -15,4 +15,29 @@ public partial class DragAndDrop : Node
 
     [Export]
     private Area2D Target { get; set; }
+
+    private Vector2 startingPosition;
+    private bool isDragging = false;
+    private Vector2 dragOffset;
+
+    public override void _Ready()
+    {
+        if (Target != null)
+        {
+            Target.InputEvent += OnInputEvent;
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        if (isDragging && Target != null)
+        {
+            Target.GlobalPosition = Target.GetGlobalMousePosition() + dragOffset;
+        }
+    }
+
+    private void OnInputEvent(Node viewport, InputEvent @event, long shapeIdx)
+    {
+        throw new NotImplementedException();
+    }
 }
